59e91d59379dbeb433cc462b7d2c4516b899cdd8
SuzukiNaoto0422 <naojin.naojin@gmail.con>
2022-12-10 16:17:46 +0900

    第2回課題 2022.12.10変更

--- src/Cat.java
+++ src/Cat.java
@@ -3,12 +3,14 @@ public class Cat {
     private String catBreed;         //猫の種類
     private String likeFood;         //好きな食べ物
     private int age;                 //年齢
+    private String eyeColor;         //瞳の色
 
-    public Cat(String name, String catBreed, String likeFood, int age) {
+    public Cat(String name, String catBreed, String likeFood, int age, String eyeColor) {
         this.name = name;
         this.catBreed = catBreed;
         this.likeFood = likeFood;
         this.age = age;
+        this.eyeColor = eyeColor;
 
     }
 
@@ -26,6 +28,10 @@ public class Cat {
 
     public int getAge() {
         return age;
+    }
+
+    public String getEyeColor() {
+        return eyeColor;
     }
 
     public void introduction() {
